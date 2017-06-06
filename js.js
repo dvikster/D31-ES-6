9ceecc56ffe7390c3d498e1e6b530def62d4b8ff
dvikster <dvikster@gmail.com>
2017-06-06 10:54:18 +0300

    added files

--- js.js
+++ js.js
@@ -42,10 +42,18 @@ data = {
 let getData = () => {document.getElementById('out3').innerHTML =`<div class="block-comments"><p class="name">${data.name}</p><img src="${data.foto}" alt=""><p class="comments">${data.comments}</p><p class="date">${data.date}</p></div>`}
 getData();
 
-// function getData() {
-//     let out = `<div class="block-comments"><p class="name">${data.name}</p><img src="${data.foto}" alt=""><p class="comments">${data.comments}</p><p class="date">${data.date}</p></div>`
-//     document.getElementById('out3').innerHTML = out;
-// }
-// getData();
+data1 = {
+    '1':{'name':'Иванов Иван',
+        'foto':'https://cdn0.iconfinder.com/data/icons/yooicons_set09_halloween/128/cheshire_cat.png',
+        'comments':'Все супер - мне понравилось',
+        'date':'2 июня 2017'}
+};
+function getData1() {
+    let out;
+    for( var key in data1)
+    out = `<div class="block-comments"><p class="name">${data1[key].name}</p><img src="${data1[key].foto}" alt=""><p class="comments">${data1[key].comments}</p><p class="date">${data1[key].date}</p></div>`
+    document.getElementById('out4').innerHTML = out;
+}
+getData1();
 
 
